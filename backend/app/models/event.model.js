cba043e243b54db06f2b3c7243e3e33b212e2081
Joss Dixon <josscdixon@gmail.com>
2022-03-18 15:59:23 +0000

    model file

---
+++ backend/app/models/event.model.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, Sequelize) => {
+  const Event = sequelize.define("event", {
+    id: {
+      type: Sequelize.STRING
+    },
+    event_type: {
+      type: Sequelize.STRING
+    },
+    visit_id: {
+      type: Sequelize.STRING
+    },
+    timestamp: {
+      type: Sequelize.STRING
+    },
+    caregiver_id: {
+      type: Sequelize.STRING
+    },
+    care_recipient_id: {
+      type: Sequelize.STRING
+    },
+    mood: {
+      type: Sequelize.STRING
+    }
+  });
+  return Tutorial;
+};
+
+// {
+//   "id": "decaa026-2ce5-49cb-aff9-92326b85a98c",
+//     "event_type": "mood_observation",
+//       "visit_id": "39b94aab-cc35-4874-807f-c23472aec663",
+//         "timestamp": "2019-04-23T10:53:13+01:00",
+//           "caregiver_id": "4786d616-259e-4d52-80f7-8cf7dc6d881a",
+//             "care_recipient_id": "03f3306d-a4a3-4179-ab88-81af66df8b7c",
+//               "mood": "okay",
+// },
