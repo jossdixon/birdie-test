852213bf15513760e34baa447b5ecf74ad29e6ae
Joss Dixon <josscdixon@gmail.com>
2022-03-23 15:17:23 +0000

    filter feeding

--- frontend/src/components/EventCard.js
+++ frontend/src/components/EventCard.js
@@ -13,14 +13,14 @@ const EventCard = ({ event }) => {
     return str.join(' ');
   }
 
-  const entries = Object.entries(event).filter(e => !e.includes('id') && !e.includes('timestamp'));
-  console.log(entries);
-
-  // const headers = Object.keys(event).filter(e => !e.includes('id') && !e.includes('timestamp'));
-
-  // const vals = event.map((e) => {
-  //   return headers.map(i => e[i]);
-  // })
+  const keys = Object.keys(event).filter((e) => {
+    // Filter out anything that is an id, is not a string or number
+    return !e.includes('_id')
+    // && !e.includes('timestamp')
+    && !e.includes('type')
+    && typeof event[e] === 'string'
+  });
+  console.log(keys);
 
   return(
     <div>
@@ -30,6 +30,20 @@ const EventCard = ({ event }) => {
             <p>{niceDate(event.timestamp)}</p>
         </div>
         <div className="card-body">
+          <div className="row justify-content-space-between">
+            {keys.map((k, i) => (
+              <div key={i} className="col-sm">
+                <strong><p>{titleText(k)}</p></strong>
+              </div>
+            ))}
+          </div>
+          <div className="row">
+            {keys.map((v, i) => (
+              <div key={i} className="col-sm">
+                <p>{titleText(event[v])}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
